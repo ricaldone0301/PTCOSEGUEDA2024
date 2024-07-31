8baea72a351d5bb8f124352615aa585f41f5591d
DanielaVillalta <20240286@ricaldone.edu.sv>
2024-07-31 12:09:21 -0600

    creé mi rama de nuevo

---
+++ Vista/Pacientes/VistaAgregarPadecimientos.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PTC.Vista.Pacientes
+{
+    partial class VistaAgregarPadecimientos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VistaAgregarPadecimientos));
+            this.bunifuCards1 = new Bunifu.Framework.UI.BunifuCards();
+            this.btnGuardarOcupacion = new System.Windows.Forms.Button();
+            this.txtNombreOcupacion = new System.Windows.Forms.TextBox();
+            this.bunifuCustomLabel4 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuGradientPanel2 = new Bunifu.Framework.UI.BunifuGradientPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuCards1.SuspendLayout();
+            this.bunifuGradientPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuCards1
+            // 
+            this.bunifuCards1.BackColor = System.Drawing.Color.White;
+            this.bunifuCards1.BorderRadius = 5;
+            this.bunifuCards1.BottomSahddow = true;
+            this.bunifuCards1.color = System.Drawing.Color.Tomato;
+            this.bunifuCards1.Controls.Add(this.bunifuGradientPanel2);
+            this.bunifuCards1.Controls.Add(this.btnGuardarOcupacion);
+            this.bunifuCards1.Controls.Add(this.txtNombreOcupacion);
+            this.bunifuCards1.Controls.Add(this.bunifuCustomLabel4);
+            this.bunifuCards1.LeftSahddow = false;
+            this.bunifuCards1.Location = new System.Drawing.Point(21, 49);
+            this.bunifuCards1.Name = "bunifuCards1";
+            this.bunifuCards1.RightSahddow = true;
+            this.bunifuCards1.ShadowDepth = 20;
+            this.bunifuCards1.Size = new System.Drawing.Size(478, 205);
+            this.bunifuCards1.TabIndex = 73;
+            // 
+            // btnGuardarOcupacion
+            // 
+            this.btnGuardarOcupacion.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnGuardarOcupacion.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarOcupacion.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.btnGuardarOcupacion.Location = new System.Drawing.Point(357, 142);
+            this.btnGuardarOcupacion.Name = "btnGuardarOcupacion";
+            this.btnGuardarOcupacion.Size = new System.Drawing.Size(100, 50);
+            this.btnGuardarOcupacion.TabIndex = 70;
+            this.btnGuardarOcupacion.Text = "GUARDAR";
+            this.btnGuardarOcupacion.UseVisualStyleBackColor = false;
+            // 
+            // txtNombreOcupacion
+            // 
+            this.txtNombreOcupacion.Location = new System.Drawing.Point(16, 88);
+            this.txtNombreOcupacion.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNombreOcupacion.Name = "txtNombreOcupacion";
+            this.txtNombreOcupacion.Size = new System.Drawing.Size(430, 20);
+            this.txtNombreOcupacion.TabIndex = 71;
+            // 
+            // bunifuCustomLabel4
+            // 
+            this.bunifuCustomLabel4.AutoSize = true;
+            this.bunifuCustomLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuCustomLabel4.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel4.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.bunifuCustomLabel4.Location = new System.Drawing.Point(12, 57);
+            this.bunifuCustomLabel4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.bunifuCustomLabel4.Name = "bunifuCustomLabel4";
+            this.bunifuCustomLabel4.Size = new System.Drawing.Size(78, 19);
+            this.bunifuCustomLabel4.TabIndex = 70;
+            this.bunifuCustomLabel4.Text = "NOMBRE";
+            // 
+            // bunifuGradientPanel2
+            // 
+            this.bunifuGradientPanel2.BackColor = System.Drawing.Color.Tomato;
+            this.bunifuGradientPanel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel2.BackgroundImage")));
+            this.bunifuGradientPanel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel2.Controls.Add(this.label2);
+            this.bunifuGradientPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bunifuGradientPanel2.GradientBottomLeft = System.Drawing.Color.Tomato;
+            this.bunifuGradientPanel2.GradientBottomRight = System.Drawing.Color.Tomato;
+            this.bunifuGradientPanel2.GradientTopLeft = System.Drawing.Color.Tomato;
+            this.bunifuGradientPanel2.GradientTopRight = System.Drawing.Color.Tomato;
+            this.bunifuGradientPanel2.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel2.Margin = new System.Windows.Forms.Padding(2);
+            this.bunifuGradientPanel2.Name = "bunifuGradientPanel2";
+            this.bunifuGradientPanel2.Quality = 10;
+            this.bunifuGradientPanel2.Size = new System.Drawing.Size(478, 24);
+            this.bunifuGradientPanel2.TabIndex = 68;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NUEVO PADECIMIENTO";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(494, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 72;
+            this.pictureBox1.TabStop = false;
+            // 
+            // VistaAgregarPadecimientos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 300);
+            this.Controls.Add(this.bunifuCards1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "VistaAgregarPadecimientos";
+            this.Text = "VistaAgregarPadecimientos";
+            this.bunifuCards1.ResumeLayout(false);
+            this.bunifuCards1.PerformLayout();
+            this.bunifuGradientPanel2.ResumeLayout(false);
+            this.bunifuGradientPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuCards bunifuCards1;
+        private Bunifu.Framework.UI.BunifuGradientPanel bunifuGradientPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGuardarOcupacion;
+        public System.Windows.Forms.TextBox txtNombreOcupacion;
+        public Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Vista/Pacientes/VistaAgregarPadecimientos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTC.Vista.Pacientes
+{
+    public partial class VistaAgregarPadecimientos : Form
+    {
+        public VistaAgregarPadecimientos()
+        {
+            InitializeComponent();
+        }
+    }
+}
